3df27f370513ce6a54fab9455b6c557da34a40de
Dennis Dijkstra <mail@dennisdijkstra.com>
2024-01-12 17:20:36 +0100

    Create first endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your controllers here.
+@api_view(['GET'])
+def getUsers(request):
+  person = {
+    'name': 'Dennis',
+    'age': 28
+  }
+
+  return Response(person)
