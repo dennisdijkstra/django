19c90046595866b7eb8e90053d4f8e633d41dc04
Dennis Dijkstra <mail@dennisdijkstra.com>
2024-01-16 14:24:26 +0100

    Add parameters to endpoints

--- users/views.py
+++ users/views.py
@@ -1,19 +1,23 @@
+import logging
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
-def getUsers():
+def getUsers(request):
+  print(request, id)
+
   person = {
     'name': 'Dennis',
     'age': 28
   }
-
   return Response(person)
 
 @api_view(['PATCH'])
-def updateUser():
-  return Response('Patch user')
+def updateUser(request, id):
+  print(request, id)
+  return Response(id)
 
 @api_view(['DELETE'])
-def deleteUser():
-  return Response('Delete user')
+def deleteUser(request, id):
+  print(request, id)
+  return Response(id)
